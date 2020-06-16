8743139f163cea7a22702ce5df9f99d04e1278fc
SaheeqAhmad <SaheeqAhmad1@gmail.com>
2020-06-16 14:42:20 +0500

    Initial Commit

---
+++ src/Child.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Child(props) {
+    return (
+        <div>
+            Child
+            This is the Number {props.num}
+        </div>
+    );
+}
+
+export default Child;
